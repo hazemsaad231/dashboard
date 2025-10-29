8edd5944c57ab6700a003a8f9846f501575406cf
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-10-29 17:26:39 +0300

    go

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,7 +3,6 @@ import { TiThMenu } from "react-icons/ti";
 import { FaHouse} from "react-icons/fa6"
 import { GrServices } from "react-icons/gr"
 import { TbLogs } from "react-icons/tb"
-import { RxAvatar } from "react-icons/rx"
 import { FaBriefcase } from "react-icons/fa";
 import { Link } from "react-router-dom"
 import Close from "../Close/close";
@@ -41,16 +40,18 @@ const Navbar = () => {
       <div className="fixed z-10 min-h-screen w-52 bg-gradient-to-b from-slate-50 to-slate-100 border-l border-slate-200 p-5 text-slate-700 hidden sm:hidden md:hidden lg:block xl:block shadow-md">
         <div className="flex flex-col items-center h-full ">
           <div className="mb-8 text-center">
-            <div className="w-24 h-24 mx-auto mb-3 rounded-full bg-gradient-to-br from-[#412da3] to-[#3318b8] p-1 shadow-md">
-              <div className="w-full h-full rounded-full bg-slate-100 flex items-center justify-center">
-                <RxAvatar size={80} className="text-[#2e268a]" />
+                <div className="w-28 h-28 mx-auto mb-3 rounded-full bg-white p-1 shadow-lg">
+              <div className="w-full h-full rounded-full bg-slate-200 flex items-center justify-center">
+                <a href="https://tadbeer-nine.vercel.app">
+                 <img src='/logo.png' alt="logo" className="w-20 h-20" />
+                 </a>
               </div>
             </div>
             <h3 className="font-bold text-lg text-slate-800">المسؤول</h3>
             <p className="text-xs text-slate-500 mt-1">مدير النظام</p>
           </div>
 
-          <ul className="flex flex-col gap-3">
+          <ul className="flex flex-col justify-center gap-3">
             {navItems.map((item) => {
               const Icon = item.icon
               return (
@@ -97,7 +98,9 @@ const Navbar = () => {
           <div className="text-center mt-12 mb-8">
             <div className="w-28 h-28 mx-auto mb-3 rounded-full bg-white p-1 shadow-lg">
               <div className="w-full h-full rounded-full bg-slate-200 flex items-center justify-center">
-                <RxAvatar size={100} className="text-[#372c70]" />
+                <a href="https://tadbeer-nine.vercel.app">
+                 <img src='/logo.png' alt="logo" className="w-20 h-20" />
+                 </a>
               </div>
             </div>
             <h3 className="text-xl font-bold">المسؤول</h3>
@@ -128,11 +131,6 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-
-      {/* Mobile Overlay */}
-      {/* {isNavbarVisible && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden" onClick={toggleNavbar} />
-      )} */}
     </>
   )
 }
